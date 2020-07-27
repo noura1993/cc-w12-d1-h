537e751e0bedafab1d65d210fddbb3fac3ca78da
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-27 16:35:57 +0100

    Add tests for getters

---
+++ Employee/src/test/java/staff/techStaff/DeveloperTest.java
@@ -0,0 +1,33 @@
+package staff.techStaff;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before() {
+        developer = new Developer("Rose", "DF 45 67 89 E", 3000);
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Rose", developer.getName());
+    }
+
+    @Test
+    public void getNationalInsuranceNumber() {
+        assertEquals("DF 45 67 89 E", developer.getNationalInsuranceNumber());
+    }
+
+    @Test
+    public void getSalary() {
+        assertEquals(3000, developer.getSalary(), 0.001);
+    }
+
+
+}
